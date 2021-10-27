e1f84a5d993e9fb55f85e4679532058920196841
Ahmet Tügür <ahmet_tgr@hotmail.com>
2021-10-27 23:14:11 +0300

    AddToCartCommandValidator updated.

--- src/TechCase.Application/Carts/AddToCart/AddToCartCommandValidator.cs
+++ src/TechCase.Application/Carts/AddToCart/AddToCartCommandValidator.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace TechCase.Application.Carts.AddToCart
 {
-    public class AddToCartCommandValidator: AbstractValidator<AddToCartCommand>
+    public class AddToCartCommandValidator : AbstractValidator<AddToCartCommand>
     {
         public AddToCartCommandValidator()
         {
-            RuleFor(_=>_.UserId).NotEmpty().WithMessage("UserId cannot be empty");
-            RuleFor(_ => _.cartItems).NotNull().WithMessage("CartItems cannot be null");
+            RuleFor(_ => _.UserId).NotEmpty().WithMessage("UserId cannot be empty");
+            RuleFor(_ => _.cartItems).Must(CartItemsIsNotNullOrGreaterThanZero).WithMessage("CartItems cannot be null");
             RuleForEach(x => x.cartItems).ChildRules(cartItem => {
                 cartItem.RuleFor(x => x.Price).GreaterThan(0).WithMessage($"Price cannot be zero."); ;
                 cartItem.RuleFor(x => x.Name).NotEmpty().WithMessage($"Name cannot be emtpty");
@@ -20,5 +20,48 @@ namespace TechCase.Application.Carts.AddToCart
                 cartItem.RuleFor(x => x.ProductId).GreaterThan(0).WithMessage($"Product Id cannot be zero.");
             });
         }
+
+        private bool CartItemsIsNotNullOrGreaterThanZero(List<AddToCartCommandCartItem> cartItems)
+        {
+            if (cartItems == null || cartItems.Count == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
+
+
+//            RuleFor(_ => _.cartItems).NotNull().WithMessage("CartItems cannot be null");
+//When(_ => _.cartItems.Count > 0, () =>
+//{
+//    RuleForEach(x => x.cartItems).ChildRules(cartItem =>
+//    {
+//        cartItem.RuleFor(x => x.Price).GreaterThan(0).WithMessage($"Price cannot be zero.");
+//        cartItem.RuleFor(x => x.Name).NotEmpty().WithMessage($"Name cannot be emtpty");
+//        cartItem.RuleFor(x => x.StockQuantity).GreaterThan(0).WithMessage($"StockQuantity  cannot be zero."); ;
+//        cartItem.RuleFor(x => x.ProductId).GreaterThan(0).WithMessage($"Product Id cannot be zero.");
+//    });
+//}).Otherwise(() =>
+//{
+//    RuleFor(_ => _.cartItems.Count).Equal(0).WithMessage($"cartItems cannot be zero.");
+//});
+//RuleFor(_ => _.cartItems).NotNull()
+//    .WithMessage("CartItems cannot be null")
+//    .Must((_, cartItems) => cartItems?.Count <= 0)
+//    .WithMessage(@"CartItems count connot be zero");
+
+//When(_ => _.cartItems != null, () => { })
+//.Otherwise(() =>
+//{
+//    RuleFor(_ => _.cartItems.Count).GreaterThan(0).WithMessage("CartItems count connot be zero");
+//});
+//RuleForEach(x => x.cartItems).ChildRules(cartItem =>
+//{
+//    cartItem.RuleFor(x => x.Price).GreaterThan(0).WithMessage($"Price cannot be zero."); ;
+//    cartItem.RuleFor(x => x.Name).NotEmpty().WithMessage($"Name cannot be emtpty");
+//    cartItem.RuleFor(x => x.StockQuantity).GreaterThan(0).WithMessage($"StockQuantity  cannot be zero."); ;
+//    cartItem.RuleFor(x => x.ProductId).GreaterThan(0).WithMessage($"Product Id cannot be zero.");
+//});
